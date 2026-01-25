c9413a6b30e5786487fa7d2ed75ccf63d3a2f146
raviya9090 <unboxx1234@gmail.com>
2026-01-25 10:11:25 +0530

    check commit

--- src/java/UTILS/DBConnection.java
+++ src/java/UTILS/DBConnection.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package UTILS;
+//package com.oceanview.util;
+ //this is error one to test commit
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @author Ashen Samarasekera
- */
 public class DBConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/ocean_view_resort";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = ""; // Change this to your MySQL password
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("MySQL JDBC Driver not found", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
